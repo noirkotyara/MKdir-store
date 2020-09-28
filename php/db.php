6195867f0767692998a446ea68c7397084d0417c
noirkotyara <53192867+noirkotyara@users.noreply.github.com>
2020-09-28 18:21:36 +0300

    l

--- php/db.php
+++ php/db.php
@@ -1,8 +1,13 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "comments";
-    $conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed" . mysqli_connect_error());
-        // echo 'Connected!';
+   // $servername = "localhost";
+    
+    // $username = "root";
+    // $password = "";
+    // $dbname = "comments";
+    // $conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed" . mysqli_connect_error());
+    // echo 'Connected!';
+
+    $db = parse_url(getenv('DATABASE_URL'));
+    $db["path"] = ltrim($db["path"], "/");
+    $conn = pg_connect(getenv("DATABASE_URL"));
 ?>
