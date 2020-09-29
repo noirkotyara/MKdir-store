4df25e22d44676c9e2473e18b99e1a36f38637f3
noirkotyara <53192867+noirkotyara@users.noreply.github.com>
2020-09-29 20:48:42 +0300

    db2

--- php/db.php
+++ php/db.php
@@ -11,21 +11,21 @@
     // $conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed" . mysqli_connect_error());
     // echo 'Connected!';
 
-    try{
-        //Set DSN data source name
-          $dsn = "pgsql:host=" . $host . ";port=" . $port .";dbname=" . $dbname . ";user=" . $user . ";password=" . $password . ";";
+    // try{
+    //     //Set DSN data source name
+    //       $dsn = "pgsql:host=" . $host . ";port=" . $port .";dbname=" . $dbname . ";user=" . $user . ";password=" . $password . ";";
       
       
-        //create a pdo instance
-        $conn = new PDO($dsn, $user, $password);
-        $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
-        $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      }
-      catch (PDOException $e) {
-      echo 'Connection failed: ' . $e->getMessage();
-      }
-
+    //     //create a pdo instance
+    //     $conn = new PDO($dsn, $user, $password);
+    //     $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+    //     $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+    //     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //   }
+    //   catch (PDOException $e) {
+    //   echo 'Connection failed: ' . $e->getMessage();
+    //   }
+    $conn = pg_connect("host=ec2-54-166-251-173.compute-1.amazonaws.com port=5432 dbname=d8lld96ssve5u user=qfjvfpkrdfgsdc password=39065255c1d33631bf6189b29aecf08492db721b757a1a385606ff327974710c");
     // $db = parse_url(getenv('DATABASE_URL'));
     // $db["path"] = ltrim($db["path"], "/");
     // $conn = pg_connect(getenv("DATABASE_URL"));
