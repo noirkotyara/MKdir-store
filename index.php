7763741ebd134c0e10ccc6db0101d70e9da885af
noirkotyara <53192867+noirkotyara@users.noreply.github.com>
2020-09-29 21:35:20 +0300

    add

--- index.php
+++ index.php
@@ -19,6 +19,8 @@
             $sql3 = "SELECT * FROM comments";
             $comments = pg_query($conn, $sql3);
             echo "Correct!";
+         }else{
+            echo "Uncorrect!";
          }
     
     
